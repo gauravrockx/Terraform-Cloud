9d093d500f2e23a9323340d5619ac53d13e8c29c
gauravrockx <gaurav.aix1@gmail.com>
2023-07-06 18:05:53 +0530

    RG Changed

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  resource_group_name ="app-rg"
+  resource_group_name ="test-rg"
   location = "West Europe"
   virtual_network = "app-network"
   virtual_network_address_space = "10.0.0.0/16"
