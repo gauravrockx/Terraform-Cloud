29c3506e53c45e39099a6a3529230f9d2fe68654
gauravrockx <gaurav.aix1@gmail.com>
2023-07-07 09:38:51 +0530

    Adding another subnet

--- virtualnetwork.tf
+++ virtualnetwork.tf
@@ -19,3 +19,10 @@ resource "azurerm_subnet" "subnetA" {
  depends_on = [ azurerm_virtual_network.appnetwork ]
 }
 
+resource "azurerm_subnet" "subnetB" {
+  name                 = "SubnetB"
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = local.virtual_network
+  address_prefixes     = [cidrsubnet(local.virtual_network_address_space,8,1)]
+ depends_on = [ azurerm_virtual_network.appnetwork ]
+}
